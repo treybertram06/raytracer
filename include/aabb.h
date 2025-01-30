4c8a7aa292dd28f282d8343a4622e822c0e5f73e
Trey Bertram <113378171+treybertram06@users.noreply.github.com>
2025-01-30 00:09:49 -0800

    implemented quadrilaterals

--- include/aabb.h
+++ include/aabb.h
@@ -10,7 +10,9 @@ public:
     aabb() {} // The default AABB is empty, since intervals are empty by default.
 
     aabb(const interval& x, const interval& y, const interval& z)
-      : x(x), y(y), z(z) {}
+      : x(x), y(y), z(z) {
+        pad_to_minimums();
+    }
 
     aabb(const point3& a, const point3& b) {
         // Treat the two points a and b as extrema for the bounding box, so we don't require a
@@ -25,6 +27,8 @@ public:
         x = interval(box0.x, box1.x);
         y = interval(box0.y, box1.y);
         z = interval(box0.z, box1.z);
+
+        pad_to_minimums();
     }
 
     const interval& axis_interval(int n) const {
@@ -68,6 +72,18 @@ public:
     }
 
     static const aabb empty, universe;
+
+private:
+
+    void pad_to_minimums() {
+        //adjust aabb so that no side is narrower than some delta
+
+        double delta = 0.0001;
+        if (x.size() < delta) x = x.expand(delta);
+        if (y.size() < delta) y = y.expand(delta);
+        if (z.size() < delta) z = z.expand(delta);
+
+    }
 
 
 };
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "include/hittable.h"
 #include "include/hittable_list.h"
 #include "include/material.h"
+#include "include/quad.h"
+#include "include/triangles.h"
 #include "include/sphere.h"
 #include "include/camera.h"
 #include "include/bvh.h"
@@ -166,13 +168,87 @@ void perlin_spheres() {
 
 }
 
+void quads() {
+    hittable_list world;
+
+    // Materials
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+
+    camera cam;
+
+    cam.image_width         = 640;
+    cam.image_height        = 360;
+    cam.samples_per_pixel   = 50;
+    cam.max_depth           = 25;
+
+    cam.vfov     = 80;
+    cam.lookfrom = point3(0,0,9);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+    cam.focus_dist    = 10.0;
+
+    cam.render(world);
+}
+
+//does not work:(
+void tris() {
+    hittable_list world;
+
+    // Materials
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<triangles>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<triangles>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<triangles>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<triangles>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<triangles>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+
+    camera cam;
+
+    cam.image_width         = 640;
+    cam.image_height        = 360;
+    cam.samples_per_pixel   = 50;
+    cam.max_depth           = 25;
+
+    cam.vfov     = 80;
+    cam.lookfrom = point3(0,0,9);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+    cam.focus_dist    = 10.0;
+
+    cam.render(world);
+}
+
 int main() {
 
-    switch (4) {
-        case 1: bouncing_spheres();  break;
-        case 2: checkered_spheres(); break;
-        case 3: earth(); break;
-        case 4: perlin_spheres(); break;
+    switch (6) {
+        case 1: bouncing_spheres();     break;
+        case 2: checkered_spheres();    break;
+        case 3: earth();                break;
+        case 4: perlin_spheres();       break;
+        case 5: quads();                break;
+        case 6: tris();                break;
+
     }
 
     return 0;
